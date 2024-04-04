503ff2b17b1b1034a22417dd354776c16eeda1d9
Mahama Hamida <mahamahamida@gmail.com>
2024-04-04 22:26:32 +0000

    add some utils functions

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+export default function getFullYear() {
+    let date = new Date;
+    return date.getFullYear()
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex)
+        return "Holberton School"
+    else
+        return "Holberton School main dashboard"
+}
