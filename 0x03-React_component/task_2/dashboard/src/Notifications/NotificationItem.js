bb93a40963d43be85776dcc0b00e86b200332ace
Mahama Hamida <mahamahamida@gmail.com>
2024-04-19 07:43:21 +0000

    add all files for task 2

---
+++ 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Notifications.css";
+import PropTypes from "prop-types";
+
+
+export default class NotificationItem extends React.Component {
+  render() {
+    const {type,html,value, markAsRead, id} = this.props
+    return (
+      <>
+        {type && value ? <li onClick={()=>markAsRead(id)} data-notification-type={type} >{value}</li> : null}
+        {html ? (
+          <li onClick={()=>markAsRead(id)} data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+        ) : null}
+      </>
+    );
+  }
+}
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  value: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+NotificationItem.defaultProps = {
+  type: "default",
+};
