4ea334339f94a8d2e5d5e6bdd3efeb425db4461b
Mahama Hamida <mahamahamida@gmail.com>
2024-04-21 22:14:53 +0000

    add body section margin

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px',
+    },
+});
 export default class BodySection extends React.Component {
     render() {
         const {title,children} = this.props;
         return (
-            <div className='bodySection'>
+            <div className={css(styles.bodySectionWithMargin)}>
                 <h2>{title}</h2>
                 {children}
             </div>
