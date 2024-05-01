2c014834c8cc5584f1ee916b5cb59776c9e170d5
Mahama Hamida <mahamahamida@gmail.com>
2024-05-01 15:56:02 +0000

    Refactor variable name in 0x06-React_Immutable/8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,12 +1,12 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(grades) {
+export default function (grades) {
   const highGrades = Seq(grades)
     .filter((student) => student.score >= 70)
     .map((student) => ({
       score: student.score,
-      firsName:
-        student.firsName.charAt(0).toUpperCase() + student.firstName.slice(1),
+      firstName:
+        student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName:
         student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
