7b2ea4df06c84832134041080146b9832311e14b
Mahama Hamida <mahamahamida@gmail.com>
2024-05-01 13:57:32 +0000

    Add merge functions for concatenating and merging Immutable objects

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,9 @@
+const { List, Map } = require('immutable');
+
+export function concat(page1, page2) {
+  return List(page1).concat(List(page2));
+}
+
+export function mergeElements(page1, page2) {
+  return Map(page1).concat(Map(page2));
+}
