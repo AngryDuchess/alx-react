6a893d219a8299d95aef7eaa715f2ed9d2886a33
Mahama Hamida <mahamahamida@gmail.com>
2024-05-01 14:10:54 +0000

    Refactor merge functions in 0x06-React_Immutable/5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,5 @@
 import { List, Map } from 'immutable';
 
-export function concat(page1, page2) {
-  return List(page1).concat(List(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-export function mergeElements(page1, page2) {
-  return Map(page1).concat(Map(page2));
-}
+export const mergeElements = (page1, page2) => Map(page1).concat(Map(page2));
