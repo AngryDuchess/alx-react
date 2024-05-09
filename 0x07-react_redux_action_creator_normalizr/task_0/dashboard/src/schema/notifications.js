2f0edd3d7d3bd092a925951be79b99b4f25943ff
Mahama Hamida <mahamahamida@gmail.com>
2024-05-09 22:36:43 +0000

    chore: Remove unused code and update schema for notifications

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,15 +1,7 @@
 import * as notifications from "../../notifications.json";
-import { normalize, schema } from 'normalizr';
 
 export default function getAllNotificationsByUser(userId) {
   const filtered = notifications.filter((notification) => notification.author.id === userId)
   const getContexts = filtered.map((notification) => notification.context);
   return getContexts;
 }
-
-const user = new schema.Entity('users');
-const message = new schema.Entity('messages');
-const messageSchema = {messages: "guid"}
-const notification = new schema.Entity('notifications');
-
-
