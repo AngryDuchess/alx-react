9cbb880727da04e590dc1a4e5a3c7acaee95bef4
Mahama Hamida <mahamahamida@gmail.com>
2024-05-09 22:37:07 +0000

    chore: Update schema for notifications and normalize data

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,8 +1,18 @@
 // import { List } from "immutable";,
-import * as notifications from "../../notifications.json";
+import notifications from "../../notifications.json";
+import { normalize, schema } from "normalizr";
 
 export default function getAllNotificationsByUser(userId) {
   return notifications
     .filter((notification) => notification.author.id === userId)
     .map((notification) => notification.context);
 }
+
+const user = new schema.Entity("users");
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notifications, [notification]);
