35d1c0b04f5e153c214e49a140e125b426c9dff7
Mahama Hamida <mahamahamida@gmail.com>
2024-05-10 22:22:55 +0000

    chore: Add initial setup files and configurations for the dashboard project

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/App/App.js
@@ -0,0 +1,155 @@
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import { StyleSheet, css } from "aphrodite";
+import { AppContext, defaultUser } from "./AppContext";
+
+const styles = StyleSheet.create({
+  App: {
+    height: "100vh",
+    maxWidth: "100vw",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
+
+  Top: {
+    "@media (min-width: 600px)": {
+      display: "flex",
+      flexDirection: "row-reverse",
+      justifyContent: "space-between",
+    },
+  },
+  hr: {
+    height: "3px",
+    backgroundColor: "#e0354b",
+  },
+  "App-footer": {
+    textAlign: "center",
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+      user: defaultUser,
+      logOut: () => {
+        this.setState({ user: defaultUser });
+      },
+      listNotifications: [
+        { id: 1, type: "default", value: "New course available" },
+        { id: 2, type: "urgent", value: "New resume available" },
+        { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+      ],
+    };
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+  }
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+  // listNotifications = [
+  //   { id: 1, type: "default", value: "New course available" },
+  //   { id: 2, type: "urgent", value: "New resume available" },
+  //   { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+  // ];
+  handleKeyPress(e) {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  logIn(email, password) {
+    this.setState({
+      user: { email: email, password: password, isLoggedIn: true },
+    });
+  }
+
+  markNotificationAsRead(id) {
+    // console.log("clicked!");
+    this.setState({
+      listNotifications: this.state.listNotifications.filter(
+        (notification) => notification.id !== id
+      ),
+    });
+  }
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  render() {
+    // const { isLoggedIn } = this.props;
+    const { displayDrawer, user, logOut, listNotifications } = this.state;
+    return (
+      <AppContext.Provider value={{ user, logOut }}>
+        <div className={css(styles.App)}>
+          <div className={css(styles.Top)}>
+            <Notifications
+              displayDrawer={displayDrawer}
+              // listNotifications={this.listNotifications}
+              listNotifications={listNotifications}
+              handleDisplayDrawer={this.handleDisplayDrawer}
+              handleHideDrawer={this.handleHideDrawer}
+              markNotificationAsRead={this.markNotificationAsRead}
+            />
+            <Header />
+          </div>
+          <hr className={css(styles.hr)} />
+          {user.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login logIn={this.logIn} />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the School">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              et neque ex. Praesent eleifend ultrices leo, non elementum ligula
+              porta ac. Sed orci ex, venenatis at efficitur sit amet, gravida id
+              nulla. Class aptent taciti sociosqu ad litora torquent per conubia
+              nostra, per inceptos himenaeos.
+            </p>
+          </BodySection>
+          <hr className={css(styles.hr)} />
+          <Footer className={css(styles["App-footer"])} />
+        </div>
+      </AppContext.Provider>
+    );
+  }
+}
+
+// App.defaultProps = {
+//   isLoggedIn: false,
+//   logOut: () => {
+//     return;
+//   },
+// };
+
+// App.propTypes = {
+//   isLoggedIn: PropTypes.bool,
+//   logOut: PropTypes.func,
+// };
+export default App;
---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/schema/notifications.js
@@ -0,0 +1,25 @@
+import notifications from "../../notifications.json";
+import { normalize, schema } from "normalizr";
+
+const user = new schema.Entity("users");
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notifications, [notification]);
+
+export default function getAllNotificationsByUser(userId) {
+  const filtered = [];
+  const messages = normalizedData.entities.messages;
+  const notifications = normalizedData.entities.notifications;
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      filtered.push(messages[notifications[id].context]);
+    }
+  }
+  return filtered;
+}
+
+console.log(getAllNotificationsByUser('5debd764a7c57c7839d722e9'));
