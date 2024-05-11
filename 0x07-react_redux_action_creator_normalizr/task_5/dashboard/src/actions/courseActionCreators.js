3619968a656a8f04021c5f9c150a808880f75229
Mahama Hamida <mahamahamida@gmail.com>
2024-05-11 00:04:53 +0000

    chore: Add UI action creators, actionTypes, and tests for login, logout, and notification drawer

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,11 @@
+import * as actions from "./courseActionTypes";
+
+export const selectCourse = (index) => ({
+  type: actions.SELECT_COURSE,
+  index
+});
+
+export const unSelectCourse =(index) => ({
+    type: actions.UNSELECT_COURSE,
+    index
+})
